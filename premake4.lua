eec4f09318a131659255c2df686a6fc6c6cefc16
DeadMG <none@none>
2013-11-27 18:27:44 +0000

    Another attempt at teamcity

--- premake4.lua
+++ premake4.lua
@@ -46,7 +46,7 @@ function AddClangDependencies(conf)
             libdirs({ _OPTIONS["llvm-path"] .. "build/" .. llvmconf .. "/lib" })
         end
     else
-        includedirs({ _OPTIONS["llvm-path"] })
+        includedirs({ "`llvm-config --includedir`" })
     end
     local clanglibs = { 
         "clangFrontend",
