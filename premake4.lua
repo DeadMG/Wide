33655281a141bda01a874e117c42ea87a2ea95b3
DeadMG <none@none>
2014-02-19 14:30:26 +0000

    Fix for build script on Linux I think

--- premake4.lua
+++ premake4.lua
@@ -300,7 +300,7 @@ WideProjects = {
             if os.is("windows") then
                 postbuildcommands ({ "(robocopy /mir \"Standard\" \"../Deployment/WideLibrary/Standard\") ^& IF %ERRORLEVEL% LEQ 1 exit 0" })
             else
-                postbuildcommands ({ "cp -r ../WideLibrary ../Deployment/WideLibrary" })
+                postbuildcommands ({ ""mkdir -p ../Deployment/WideLibrary", "cp -r ../WideLibrary ../Deployment/WideLibrary" })
             end
         end
     },
