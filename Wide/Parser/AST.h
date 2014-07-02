2168672914887c09972b7b64defbcf7aa57d58c5
DeadMG <none@none>
2014-07-02 18:32:39 +0100

    Reverted bad change from before and bad AST.

--- Wide/Parser/AST.h
+++ Wide/Parser/AST.h
@@ -163,7 +163,6 @@ namespace Wide {
             DynamicFunction(std::vector<Statement*> b, std::vector<FunctionArgument> args, Lexer::Range loc, std::vector<Attribute> attributes)
             : AttributeFunctionBase(std::move(b), loc, args, std::move(attributes)) {}
             bool dynamic = false;
-            bool defaulted = false;
         };
         struct Function : DynamicFunction {
             Function(std::vector<Statement*> b, Lexer::Range loc, std::vector<FunctionArgument> ar, Expression* explicit_ret, std::vector<Attribute> attributes)
