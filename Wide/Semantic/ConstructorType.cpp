42a4c23b9785576618d981b16753049c2a129870
DeadMG <wolfeinstein@gmail.com>
2015-02-23 17:51:27 +0000

    Fixed up error handling of ConstructorType

--- Wide/Semantic/ConstructorType.cpp
+++ Wide/Semantic/ConstructorType.cpp
@@ -20,10 +20,10 @@ namespace {
 
         std::shared_ptr<Expression> ConstructCall(Expression::InstanceKey key, std::shared_ptr<Expression> self, std::vector<std::shared_ptr<Expression>> args, Context c) override final {
             if (args.size() == 0)
-                throw std::runtime_error("Attempted to make an array without passing a size.");
+                throw Error(c.where, "Attempted to make an array without passing a size.");
             auto constructed = t->GetConstructedType();
             if (!args[0]->IsConstantExpression(key))
-                throw std::runtime_error("Attempted to make an array but the argument was not a constant integer.");
+                throw Error(c.where, "Attempted to make an array but the size was not a constant integer.");
             auto integer = analyzer.EvaluateConstantIntegerExpression(args[0], key);
             return BuildChain(std::move(self), analyzer.GetConstructorType(analyzer.GetArrayType(t->GetConstructedType(), integer.getLimitedValue()))->BuildValueConstruction(Expression::NoInstance(), {}, { this, c.where }));
         }
@@ -40,7 +40,7 @@ namespace {
         std::shared_ptr<Expression> AccessNamedMember(Expression::InstanceKey key, std::shared_ptr<Expression> self, std::string name, Context c) override final {
             auto member = t->GetConstructedType()->AccessStaticMember(name, c);
             if (!member)
-                throw std::runtime_error("Attempted to access nonexistent member.");
+                throw Error(c.where, "Attempted to access nonexistent static member.");
             return BuildChain(self, member);
         }
         OverloadSet* CreateOperatorOverloadSet(Parse::OperatorName what, Parse::Access access, OperatorAccess kind) {
