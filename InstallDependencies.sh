f11c991f08a6f96e597c134390fe44a2387418cf
Puppy <wolfeinstein@gmail.com>
2015-11-12 20:20:22 +0000

    Install g++4.8 before trying to set it

--- InstallDependencies.sh
+++ InstallDependencies.sh
@@ -8,7 +8,9 @@ sudo apt-get install -y ncurses-dev
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -q
 sudo apt-get install gcc-4.8 -y
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
+
+if [ "$CXX" = "g++" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
+echo $CXX --version 
 
 if ! test -e "/opt/wide" ; then
     sudo mkdir /opt/wide
