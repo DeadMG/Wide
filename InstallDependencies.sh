62bd62bab65158e06522dd03f0d2dedbca1b23dc
Puppy <wolfeinstein@gmail.com>
2015-11-12 21:35:07 +0000

    Add premake4 package source

--- InstallDependencies.sh
+++ InstallDependencies.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 sudo apt-get update -qq
-sudo apt-get install -y premake4
 sudo apt-get install -y libarchive-dev
 sudo apt-get install -y zlib1g-dev
 sudo apt-get install -y ncurses-dev
@@ -11,6 +10,9 @@ sudo apt-get install -y llvm-3.6-dev
 sudo apt-get install -y libclang-3.6-dev
 sudo apt-get install -y clang-3.6
 export CXX=clang-3.6
+sudo add-apt-repository ppa:codegear/release -y
+sudo apt-get update -q
+sudo apt-get install -y premake4 
 
 if ! test -e "/opt/wide" ; then
     sudo mkdir /opt/wide
