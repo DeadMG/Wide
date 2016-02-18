763a59fcaa0f21f16cda5357b8d1c3f68b7327ef
Puppy <wolfeinstein@gmail.com>
2016-02-18 00:28:29 +0000

    Ignore non-fatal errors; fix exported functions test

--- Wide/Util/Driver/Options.cpp
+++ Wide/Util/Driver/Options.cpp
@@ -286,7 +286,7 @@ std::string Wide::Driver::JsonResponse(const Response& r, JSONOptions opts) {
         error["Where"] = JsonRange(err.location());
         errors.push_back(error);
     }
-    for (auto&& err : r.Analysis.ClangErrors) {
+    /*for (auto&& err : r.Analysis.ClangErrors) {
         std::map<std::string, json::value> error;
         error["What"] = err.what;
         std::vector<json::value> locations;
@@ -295,7 +295,7 @@ std::string Wide::Driver::JsonResponse(const Response& r, JSONOptions opts) {
         error["Where"] = locations;
         error["Severity"] = err.severity;
         errors.push_back(error);
-    }
+    }*/
     for (auto&& err : r.Analysis.ClangFatals) {
         std::map<std::string, json::value> error;
         error["What"] = err.what;
